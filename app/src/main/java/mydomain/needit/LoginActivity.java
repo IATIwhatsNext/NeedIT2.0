f9d0926cc2a5eb52153f3328eb394657ddaa4e57
itai.team123@gmail.com <itai.team123@gmail.com>
2016-03-07 18:14:22 +0200

    login to facebook

---
+++ app/src/main/java/mydomain/needit/LoginActivity.java
@@ -0,0 +1,68 @@
+package mydomain.needit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+/**
+ * Created by 10176331 on 3/7/2016.
+ */
+public class LoginActivity extends FragmentActivity {
+    private TextView info;
+    private LoginButton loginButton;
+
+    private CallbackManager callbackManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.login_activity);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                info.setText("Login attempt failed.");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+ }
